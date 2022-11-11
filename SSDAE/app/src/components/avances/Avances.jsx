d29b5b6de46661ab94cbfc650c7d29420e5ad9b0
DiegoMoralesBravo <Diegomorales1359@gmail.com>
2022-11-11 16:12:40 -0600

    Se hace validacion de tipo de extension

--- SSDAE/app/src/components/avances/Avances.jsx
+++ SSDAE/app/src/components/avances/Avances.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
+import { useRef } from 'react';
 import { useState } from 'react';
 import { useApi } from '../../hooks/useApi'
 
-
-
 export const Avances = () => {
   const api = useApi();
+  const alert = useRef();
+  const formulario = useRef();
 
   const [file, setFile] = useState(null);
 
   const saveFile = (e) => {
     setFile(e.target.files[0]);
-    console.log(e.target.files[0])
   }
 
   const sendFile = async (e) => {
@@ -22,30 +22,35 @@ export const Avances = () => {
 
     formData.append("file", file);
 
-    console.log(formData)
-    console.log('Impresion de .file')
-    console.log(formData.file)
-
     let url = "http:///localhost:3000/avances/saveFile";
 
     let res = await api.request(url, "POST", formData, true);
 
-    console.log(res)
-    console.log('Archivo enviado');
+    if (res.mensaje == 'Archivo guardado') {
+      console.log('Archivo guardado')
+    } else {
+      alert.current.style.display = 'block';
+      alert.current.style.color = 'red';
+      alert.current.innerText = '*Solo archivos comprimidos'
+    }
+
+    formulario.current.reset();
   }
 
-  return (
-    <div className="login-page">
-      <div className="form">
-        <form className="login-form" onSubmit={sendFile} >
 
-          <input type="file" placeholder="Correo electronico" name='file' onChange={saveFile} required />
 
-          <p style={{ display: 'none' }} >*Usuario y/o contraseña incorrectos</p>
+return (
+  <div className="login-page">
+    <div className="form">
+      <form className="login-form"  ref={formulario} onSubmit={sendFile} >
 
-          <button>Subir archivo</button>
-        </form>
-      </div>
+        <input type="file" placeholder="Correo electronico" name='file' onChange={saveFile} required />
+
+        <p ref={alert} style={{ display: 'none' }} >*Usuario y/o contraseña incorrectos</p>
+
+        <button>Subir archivo</button>
+      </form>
     </div>
-  )
+  </div>
+)
 }
